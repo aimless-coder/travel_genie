79bfdb09ac6c3e59ad10d9ebe0299dcebb073e34
Dipankar Purkayastha <169923075+Aimless-Coder@users.noreply.github.com>
2025-01-19 20:06:55 +0530

    Bug Fix

--- src/view-trip/[tripID]/index.jsx
+++ src/view-trip/[tripID]/index.jsx
@@ -2,7 +2,7 @@ import { db } from "@/service/FirebaseConfig";
 import { doc, getDoc } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import InfoSection from "../components/infoSection";
+import InfoSection from "../components/InfoSection";
 import Hotels from "../components/Hotels";
 import Itinerary from "../components/Itinerary";
 
