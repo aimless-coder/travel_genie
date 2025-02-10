675c212de1b91bb06e9b3f0f0eabcfd3ce818b14
Dipankar Purkayastha <169923075+Aimless-Coder@users.noreply.github.com>
2025-02-10 19:29:34 +0530

    Final Build

--- src/components/pages/Home/comp/Footer.jsx
+++ src/components/pages/Home/comp/Footer.jsx
@@ -8,8 +8,8 @@ function Footer() {
             <div className="grid place-items-start px-14 w-[50%]">  
             <img src="/images/logo.png" alt="logo" className="h-[45px] md:h-[55px]" />
             </div>
-            <div className="w-[50%] min-h-[70px] bg-[#4b164c] rounded-l-full grid place-items-center">
-                <h2 className="text-sm text-white">&copy; {currentYear} Travel Genie. All rights reserved.</h2>
+            <div className="w-[50%] min-h-[70px] bg-[#4b164c] rounded-l-full grid place-items-center p-2">
+                <h2 className="text-xs lg:text-sm text-gray-300">&copy; {currentYear} Travel Genie.</h2>
             </div>
         </div>
     </div>
