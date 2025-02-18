d25d5081e58a93fb5a0868e0557dc528d7855af7
Dipankar Purkayastha <169923075+Aimless-Coder@users.noreply.github.com>
2025-02-18 19:10:43 +0530

    Bug Fix

--- src/components/pages/view-trip/components/Itinerary.jsx
+++ src/components/pages/view-trip/components/Itinerary.jsx
@@ -3,29 +3,21 @@ import { useEffect } from "react";
 import PlaceCard from "./PlaceCard";
 
 function Itinerary({ trip }) {
-  useEffect(() => {}, [trip])
+  useEffect(() => {}, [trip]);
+
   return (
     <div className="min-w-full">
-      <h2 className="font-bold text-lg mb-5">Places to Visit</h2>
-
-      <div className="relative py-5 max-w-full">
-
-        <div className="absolute left-5 top-0 w-1 h-full border-l-2 border-[#4b164c] border-dashed"></div>
-
-        {trip?.tripData?.tripDetail?.itinerary?.map((item, index) => (
-          <div key={index} className="w-full flex flex-col items-start mb-8 relative">
-            <div className="w-10 h-10 flex items-center justify-center bg-[#4b164c] text-white font-bold rounded-xl z-10 absolute left-0">
-              {item.dayNumber.toString().padStart(2, "0")}
+      <h2 className="font-bold text-xl mt-1.5 mb-2">Itinerary</h2>
+      <div className="flex flex-col gap-5">
+        {trip?.tripData?.itinerary?.map((day, index) => (
+          <div key={index} className="border rounded-lg p-5">
+            <div className="flex justify-between items-center">
+              <h2 className="font-bold">Day {day.dayNumber}</h2>
+              <h2 className="text-gray-500">{day.theme}</h2>
             </div>
-            <div className="ltr ps-14 py-1 w-full">
-              <h2 className="font-medium text-lg"> Day: {item.dayNumber} <span>{item.theme}</span></h2>
-              {item.places.map((place, index) => (
-                <div key={index} className="my-3 w-full">
-                  <h2 className="font-medium text-sm text-[#4b164c]">
-                    {place.bestTimeToVisit}
-                  </h2>
-                  <PlaceCard place={place} />
-                </div>
+            <div className="mt-5 flex flex-col gap-3">
+              {day.places.map((place, placeIndex) => (
+                <PlaceCard key={placeIndex} place={place} />
               ))}
             </div>
           </div>
